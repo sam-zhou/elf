2b6919a79f9b24d2308fc07a527b28a6d9728e5a
Sam Zhou <samzhou.it@gmail.com>
2017-10-04 21:38:58 +0800

    Updated swagger command

--- tools/generate-swagger-client.cmd
+++ tools/generate-swagger-client.cmd
@@ -1,3 +1,3 @@
 rmdir /s /q %~dp0\..\src\Boundlss.Coaching\App\service
 
-java -jar %~dp0\swagger-codegen-cli-2.2.3.jar generate -i http://localhost:6234/swagger/docs/v1 -l typescript-angular2 -o %~dp0\..\src\Carsales.Web\Carsales.Web\ClientApp\app\api
+java -jar %~dp0\swagger-codegen-cli-2.2.3.jar generate -i http://localhost:6234/swagger/docs/v1 -l typescript-angular2 -o %~dp0\..\src\Elf.Web\Elf.Web\ClientApp\app\api
